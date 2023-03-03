3167a117e6e5525cbb61c1869bc0a503e17066c9
Berczi Sandor <sandor.berczi@urss.hu>
2023-03-03 09:00:18 +0100

    refactoring: hun -> eng 1

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ class Tarifa:
 
         self.base = params.get("base", 'perc')
         self.alap_dij = int(params.get("alap_dij", 0))
-        self.ingyen_percek = params.get("ingyen_percek", 0)
-        self.ingyen_percek_sajat = params.get("ingyen_percek_sajat", 0)
+        self.free_mins = params.get("ingyen_percek", 0)
+        self.free_mins_net_intern = params.get("ingyen_percek_sajat", 0)
         self.ingyen_percek_eu = params.get("ingyen_percek_eu", None)
 
         self.perc_dij = int(params.get("perc_dij", 0))
@@ -61,8 +61,8 @@ class Tarifa:
         r += f"Szolg:{self.carrier}\n"
         r += f"Alapdíj:{self.alap_dij}\n"
         r += f"Számlázás alapja:{self.base}\n"
-        r += f"Ingyen percek:{self.ingyen_percek}\n"
-        r += f"Ingyen percek hálózaton belül:{self.ingyen_percek_sajat}\n"
+        r += f"Ingyen percek:{self.free_mins}\n"
+        r += f"Ingyen percek hálózaton belül:{self.free_mins_net_intern}\n"
         r += f"Percdíj: {self.perc_dij} Ft\n"
         r += f"Mobilnet: {self.netGB}GB\n"
         r += f""
@@ -73,8 +73,8 @@ class Tarifa:
             retval = ['Szolgáltató', 'Alapdíj', 'Számlázás alapja', 'Ingyen percek',
                       'Ingyen percek hál. belül', 'Ingyen percek EU', 'Percdíj', 'Mobilnet[GB]']
         else:
-            retval = [self.carrier, self.alap_dij, self.base, self.ingyen_percek,
-                      self.ingyen_percek_sajat, self.ingyen_percek_eu, self.perc_dij, self.netGB]
+            retval = [self.carrier, self.alap_dij, self.base, self.free_mins,
+                      self.free_mins_net_intern, self.ingyen_percek_eu, self.perc_dij, self.netGB]
         return retval
 
 
@@ -133,26 +133,23 @@ class CallRecord:
         self.szamolt_dij = None
 
     def __repr__(self):
-        r = "{}, {} perc, ".format(self.start, self.hossz_perc)
-        r += "irány: {},".format(self.type)
-        r += "díj: {} Ft,".format(self.szamolt_dij)
+        r = "{}, {} mins, ".format(self.start, self.hossz_perc)
+        r += "direction: {},".format(self.type)
+        r += "price: {} Ft,".format(self.szamolt_dij)
         return r
 
 
 class Bill:
     def __init__(self, tarifa: Tarifa, callRecords: List[CallRecord] = []):
-        self.reszletek = {}
+        self.details = {}
         self.tarifa = tarifa
         self.last_cr_date = None
         self.yearMonth = None
         self.callRecords = {}
         [self.add_call_record(x) for x in callRecords]
 
-        self.maradek_ingyen_percek = tarifa.ingyen_percek
-        self.maradek_ingyen_percek_sajat = tarifa.ingyen_percek_sajat
-        # self.fizetendo = {}  # self.tarifa.alap_dij
-        # self.summary = 0
-
+        self.remainig_free_mins = tarifa.free_mins
+        self.remainig_free_mins_net_intern = tarifa.free_mins_net_intern
         self.recalculate_all()
 
     def add_call_record(self, callRecord: CallRecord, do_billing=True):
@@ -160,7 +157,7 @@ class Bill:
             self.yearMonth = callRecord.yearMonth
         else:
             if self.yearMonth != callRecord.yearMonth:
-                logging.error("Ez a hívás nem ebbe a hónapba tartozik.")
+                logging.error("This call shouldn't be here.")
                 sys.exit(1)
         if callRecord.epoch in self.callRecords:
             logging.debug("Double call records for the same time???")
@@ -170,96 +167,91 @@ class Bill:
         if do_billing: self.bill(callRecord)
 
     def bill(self, callRecord: CallRecord):
-        logging.info(f"get_szamolt_dij({callRecord}, {self.tarifa.carrier} - {self.tarifa.desc})")
+        logging.info(f"bill({callRecord}, {self.tarifa.carrier} - {self.tarifa.desc})")
         cid = callRecord.id
-        if cid in self.reszletek:
-            logging.info("Már kiszámolva.")
-            return self.reszletek[callRecord.id]['fizetendo']
+        if cid in self.details:
+            logging.info("Already calculated.")
+            return self.details[callRecord.id]['fizetendo']
 
-        self.reszletek[cid] = {}
-        self.reszletek[cid]['fizetendo'] = 0
+        self.details[cid] = {}
+        self.details[cid]['fizetendo'] = 0
 
         if self.last_cr_date is not None and self.last_cr_date > callRecord.start:
             raise Exception(
-                "Error: nem időrendi sorrendben érkeznek be a rekordok, így nem lehet feldolgozni őket.\n{}".format(
+                "Error: The call records should be sorted by date, "
+                "they aren't.\n{}".format(
                     callRecord))
         self.last_cr_date = callRecord.start
-        # self.fizetendo[callRecord.id] = 0
 
         if callRecord.type.lower() != 'outgoing': return 0
         if callRecord.hossz_perc == 0: return 0
 
-        halozaton_belul = self.tarifa.carrier == callRecord.toCarrier
-        perc_dij = self.tarifa.perc_dij
-        fizetendo_perc = callRecord.hossz_perc
+        is_net_intern = self.tarifa.carrier == callRecord.toCarrier
+        price_per_min = self.tarifa.perc_dij
+        mins_to_pay = callRecord.hossz_perc
 
-        if halozaton_belul:
-            logging.info("Ez egy hálózaton belüli hívás.")
-            if self.maradek_ingyen_percek_sajat > 0:
-                if fizetendo_perc <= self.maradek_ingyen_percek_sajat:
+        if is_net_intern:
+            logging.info("Ihis is a network-intern call.")
+            if self.remainig_free_mins_net_intern > 0:
+                if mins_to_pay <= self.remainig_free_mins_net_intern:
                     # elég a keret
-                    self.maradek_ingyen_percek_sajat = \
-                        self.maradek_ingyen_percek_sajat \
+                    self.remainig_free_mins_net_intern = \
+                        self.remainig_free_mins_net_intern \
                         - \
-                        fizetendo_perc
-                    fizetendo_perc = 0
+                        mins_to_pay
+                    mins_to_pay = 0
                     logging.info(
                         "{} ingyen perc elhasználva, marad még {} ingyen perc saját hálózaton belül.".format(
-                            callRecord.hossz_perc, self.maradek_ingyen_percek_sajat))
+                            callRecord.hossz_perc, self.remainig_free_mins_net_intern))
                 else:
                     # nem elég a keret
-                    fizetendo_perc = fizetendo_perc - self.maradek_ingyen_percek_sajat
-                    self.maradek_ingyen_percek_sajat = 0
+                    mins_to_pay = mins_to_pay - self.remainig_free_mins_net_intern
+                    self.remainig_free_mins_net_intern = 0
                     logging.info(
                         "{} ingyen perc elhasználva, viszont ezzel elfogyott a saját hálózaton belüli ingyen perc, sőt, még {} elszámolandó.".format(
                             1, 2))
 
-        if fizetendo_perc > 0:
+        if mins_to_pay > 0:
             if self.maradek_ingyen_percek > 0:
-                if fizetendo_perc <= self.maradek_ingyen_percek:
+                if mins_to_pay <= self.maradek_ingyen_percek:
                     self.maradek_ingyen_percek = self.maradek_ingyen_percek - \
-                                                 fizetendo_perc
-                    fizetendo_perc = 0
+                                                 mins_to_pay
+                    mins_to_pay = 0
                     logging.info(
                         "Marad még {} ingyen perc".format(self.maradek_ingyen_percek))
                 else:
-                    fizetendo_perc = fizetendo_perc - self.maradek_ingyen_percek
+                    mins_to_pay = mins_to_pay - self.maradek_ingyen_percek
                     self.maradek_ingyen_percek = 0
                     logging.info(
                         "Ezzel elfogyott az ingyen perc, {} percet fizetni kell.".format(
-                            fizetendo_perc))
+                            mins_to_pay))
 
-        self.reszletek[cid]['fizetendo_perc'] = fizetendo_perc
-        self.reszletek[cid]['percdij'] = perc_dij
-        self.reszletek[cid]['fizetendo'] = fizetendo_perc * perc_dij
+        self.details[cid]['fizetendo_perc'] = mins_to_pay
+        self.details[cid]['percdij'] = price_per_min
+        self.details[cid]['fizetendo'] = mins_to_pay * price_per_min
 
-        return self.reszletek[cid]['fizetendo']
+        return self.details[cid]['fizetendo']
 
     def recalculate_all(self):
         for callRecord in self.callRecords:
             self.bill(callRecord)
 
-        # self.summary = sum([x.get_szamolt_dij(tarifa=self.tarifa) for x in
-        #                     self.callRecords])
-
-    def get_mindosszesen(self):
-        mindosszesen = 0
-        mindosszesen += self.tarifa.alap_dij
-        for x in self.reszletek.values():
-            if 'fizetendo' in x: mindosszesen += x['fizetendo']
-        self.mindosszesen = mindosszesen
-        return mindosszesen
+    def get_endsum(self):
+        endsum = 0
+        endsum += self.tarifa.alap_dij
+        for detail in self.details.values():
+            if 'fizetendo' in detail: endsum += detail['fizetendo']
+        self.endsum = endsum
+        return endsum
 
 
 def import_csv(fle: str):
-    # return [tuple(x) for x in df.values]
-    # return [[row[col] for col in df.columns] for row in df.to_dict('records')]
     return pd.read_csv(fle, dtype=str, delimiter=',').to_dict('records')
 
 
 class Billing:
-    def __init__(self, tarifak_file: str, call_records_file: str):
-        self.tarifak = Tarifa.load(tarifak_file)
+    def __init__(self, tariff_file: str, call_records_file: str):
+        self.tarifak = Tarifa.load(tariff_file)
         self.call_records = CallRecord.load(call_records_file)
         self.bills: Dict[Bill] = {}
         pd.set_option('display.max_columns', None)
@@ -280,7 +272,7 @@ class Billing:
         for yearMonth in self.bills:
             for tarifa in self.bills[yearMonth]:
                 bill: Bill = self.bills[yearMonth][tarifa]
-                bill.get_mindosszesen()
+                bill.get_endsum()
 
         self._create_pd_data()
 
@@ -302,7 +294,7 @@ class Billing:
             dd = []
             for ym in list(self.bills.keys()):
                 bill = self.bills[ym][tarifa]
-                dd.append(bill.get_mindosszesen())
+                dd.append(bill.get_endsum())
             data[tarifa] = dd
         self.df = pd.DataFrame(data, index=list(self.bills.keys()))
 
@@ -335,16 +327,17 @@ if __name__ == '__main__':
     input = "CallyzerBackup_2022-03-20_10_02_04"
 
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--tarif_file', '-t',
-                            help='A tarifákat tartalmazó file',
-                            default='tarifak.json')
+    arg_parser.add_argument('--tariff_file', '-t',
+                            help='The JSON file containing the tariffs.',
+                            default='tariffs.json')
     arg_parser.add_argument('--call_records_file', '-c',
-                            help='A híváslista file, amit a Callyzer generált',
+                            help='The callrecord file generated by '
+                                 'Callyzer',
                             default=f'./work/input/{input}.csv')
     args = arg_parser.parse_args()
     logging.basicConfig(level=logging.WARNING)
 
-    if not os.path.exists(args.tarif_file):
+    if not os.path.exists(args.tariff_file):
         raise FileExistsError(
             f"File {args.tarif_file} does not exist, aborting.")
         sys.exit(0)
@@ -355,7 +348,7 @@ if __name__ == '__main__':
         sys.exit(0)
 
     billing = Billing(
-        tarifak_file=args.tarif_file,
+        tariff_file=args.tariff_file,
         call_records_file=args.call_records_file
     )
     billing.create_bills()
